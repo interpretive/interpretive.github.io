e03d884142e74916f00230752468c2545176f175
Ollie Marshall <ollie.marshall1@gmail.com>
2015-01-12 00:10:05 +0000

    Several new pages for website

--- js/script.js
+++ js/script.js
@@ -1,28 +1,23 @@
+$(window).scroll(function() {
+    if($(this).scrollTop() > 60) {
+        $('.navbar-fixed-top').addClass('opaque');
+    } else {
+        $('.navbar-fixed-top').removeClass('opaque');
+    }
+});
+
 $(document).ready(function() {
     setTimeout(function() {
-      $(".typed").typed({
-        strings: [" great closers", "n't statisticians", " relationship builders", "n't data scientists"],
-        typeSpeed: 20,
-        backSpeed: 20,
-        startDelay: 200,
-        backDelay: 800,
-        showCursor: false,
-        loop: true,
-        loopCount: 2
-      });
-
-      //jQuery(function () {
-      //  jQuery('[data-typer-targets]').typer({
-      //    typerInterval     : 2000,
-      //    highlightSpeed    : 30,
-      //    typeSpeed         : 200,
-      //    clearDelay        : 500,
-      //    typeDelay         : 30,
-      //    clearOnHighlight  : true,
-      //    typerDataAttr     : 'data-typer-targets',
-      //    typerOrder        : 'sequential'
-      //  });
-      //});
+        $(".typed").typed({
+            strings: [" great closers", "n't statisticians", " relationship builders", "n't data scientists"],
+            typeSpeed: 20,
+            backSpeed: 20,
+            startDelay: 200,
+            backDelay: 800,
+            showCursor: false,
+            loop: true,
+            loopCount: 2
+        });
     }, 500);
 
     $(".screenshots").owlCarousel({
